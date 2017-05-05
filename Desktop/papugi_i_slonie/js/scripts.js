048cddb5bfb7e617d8fb35ddef7789c2805ee90f
Beata Kopka <kopka.beata@gmail.com>
2017-05-05 23:29:53 +0200

    pierwsza wersja

---
+++ Desktop/papugi_i_slonie/js/scripts.js
@@ -0,0 +1,16 @@
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+	animal = 'Zielone słonie';
+	animalUpperCased = animal.toUpperCase();
+	alert(animalUpperCased);
+
+console.log(animalUpperCased);
+
+var textCharsAfter = text.replace('Papugi', 'ZIELONE SŁONIE');
+	alert(textCharsAfter);
+
+console.log(textCharsAfter);
+
+var partOftextCharsAfter = textCharsAfter.length/2;
+
+console.log(partOftextCharsAfter);
+alert('ZIELONE SŁONIE koszą trawę, unosząc się nad z');
